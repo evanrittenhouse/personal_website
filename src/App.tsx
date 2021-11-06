9e053e62b3ffb8bd555a5b7293ca2aa7b1d8291e
Evan Rittenhouse <evanrittenhouse@gmail.com>
2021-11-06 13:57:01 -0500

    Starting to create server and frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
-import React from 'react';
+import LoginButton from './frontend/login_button';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        Evan Rittenhouse
+        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
           Learn React
         </a>
+        <LoginButton />
       </header>
     </div>
   );
---
+++ src/server/config.ts
@@ -0,0 +1,7 @@
+export const credentials = {
+    serverPort: 5000,
+    clientPort: 3000,
+    id: 'b817214b1ce74e28844cd890f5a050a3',
+    secret: '6a74579918844fbbb727d9f56e4883a8',
+    redirect: `https://localhost:5000/oauth.html` // TODO: change port name to serverPort
+};
